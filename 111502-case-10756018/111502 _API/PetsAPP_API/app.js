5a311dcf953eff437244146ecb8373d13405ef1f
Sweng <10756018@ntub.edu.tw>
2022-11-30 22:17:52 +0800

    10756018修改跑版問題以及內容出錯20221130

---
+++ 111502-case-10756018/111502 _API/PetsAPP_API/app.js
@@ -0,0 +1,47 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var API1Router=require('./routes/API1');
+var API2Router=require('./routes/API2');
+var API3Router=require('./routes/API3');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/API1', API1Router);
+app.use('/API2', API2Router);
+app.use('/API3', API3Router)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
